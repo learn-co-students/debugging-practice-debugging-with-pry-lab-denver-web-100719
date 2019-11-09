a0ee7ec1aa24211a122bbf0ae2474e2b7c103780
Parker Bacall <pbacall2@gmail.com>
2019-11-09 02:11:33 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,11 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  array = []
+  crew.each do |crew_member|
+    array.push("Hello #{crew_member}.")
+  end
+  array
 end
 
 def engage
